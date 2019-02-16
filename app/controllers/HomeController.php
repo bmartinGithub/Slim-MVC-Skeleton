4f2fc5767caab59788ba60be1e66f96584cd747e
brucemartin <bmartin@mac.com>
2019-02-16 16:54:38 -0500

    removed commented code

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -17,21 +17,12 @@ class HomeController extends BaseController
 
     public function indexAction(RequestInterface $request, ResponseInterface $response, array $args){
         if($this->csrfSafe($request)){
-
-
-//        $args['model']=$this->container->ReviewModel->getTable()->get();
-//        $args['recordCount']=$args['model']->count();
-        return $this->renderTwigView($response,$args,__FUNCTION__);
-            //$this->container->view->render($response,'home/index.twig',$args);
-//        return $this->container->renderer->render($response, 'index.phtml', $args);
+            return $this->renderTwigView($response,$args,__FUNCTION__);
         }
     }
 
     public function landingAction(RequestInterface $request, ResponseInterface $response, array $args){
-//        $args['model']=$this->container->ReviewModel->getTable()->get();
-//        $args['recordCount']=$args['model']->count();
         return $this->container->view->render($response,'home/index2.twig',$args);
-//        return $this->container->renderer->render($response, 'index.phtml', $args);
     }
 
 }
